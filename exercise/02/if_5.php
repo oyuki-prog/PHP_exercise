8c569edef98b95e0a826b9de8600d22cfb2fb8fb
oyuki-prog <oyu8no3jo@gmail.com>
2021-09-12 19:57:07 +0900

    if,elseif,elseの課題

--- exercise/02/if_5.php
+++ exercise/02/if_5.php
@@ -2,3 +2,16 @@
 
 $num = 5;
 
+$flg = true;
+for($i = 2; $i < $num; $i++) {
+  if($num % $i == 0) {
+    $flg = false;
+    break;
+  }
+}
+
+if ($flg) {
+  echo "{$num}は素数です。";
+} else {
+  echo "{$num}は素数ではありません。";
+}
